8e49576bddd6a2ede853b58f631c3113c54c1f6f
Adam Szymanowski <adam.szym@yahoo.pl>
2014-10-17 14:28:36 +0200

    First test for Car model

--- CarService/test/models/car_test.rb
+++ CarService/test/models/car_test.rb
@@ -1,7 +1,29 @@
 require 'test_helper'
 
 class CarTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  
+  test "Create car with valid hash" do
+    assert_difference("Car.count", 1) do
+      Car.create(valid_car_hash)
+    end
+  end
+
+
+
+
+
+  # helper methods
+  def valid_car_hash 
+    {
+      brand: "Ford",
+      model: "Mustang II",
+      production_year: "1963",
+      mileage: 100_000
+    }
+  end
+
+
+ 
+
+
 end

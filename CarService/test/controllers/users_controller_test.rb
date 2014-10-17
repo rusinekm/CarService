d53ab0aa6a1d9d1f9f5b62dda91f5d1fa3489b4e
Fistaszek <fistaszek.lesny@wp.pl>
2014-10-17 16:23:26 +0200

    widoki_dla_usera

--- CarService/test/controllers/users_controller_test.rb
+++ CarService/test/controllers/users_controller_test.rb
@@ -1,7 +1,12 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+   
+test 'show all users ' do 
+	get :index
+	assert_response :success
+ 	assert_template :index
+end
+ 
+
 end

3a4a4d35b00efb2724eb9391b08768fae445d1cd
Michał Konrad Rusinek <rusinekmk@gmail.com>
2014-10-24 09:27:20 +0200

    wrzucony search

--- CarService/app/models/car.rb
+++ CarService/app/models/car.rb
@@ -6,13 +6,29 @@ class Car < ActiveRecord::Base
   validates :production_year, :mileage, 
             numericality: { only_integer: true }
 
-  def isrented
+  def is_rented?
     result = false
     car.rentals.each do |rental|
-      if rental.rented_to > DateTime.now()
+      if rental.rented_to > Date.today()
         result = rental
       end
     end
     result 
   end
+
+  def self.search(params)
+  if params[:user_id]
+    ids = Rental.where(user_id: param:user_name.map(&:car_id)
+    Article.where("id in (?)", ids)
+  elsif params[:search]
+    search_words = params[:search].split(',').map(&:strip).uniq
+    seaarch_words.each do |search|
+      ids = Article.where('model LIKE ?', "%#{search}%") 
+      ids += Article.where('brand LIKE ?', "%#search]%")
+      ids += Rental.where(user_name: search).map(&:car_id)
+      Article.where("id in (?)", ids) 
+  else
+    Article.all
+  end
+end
 end
